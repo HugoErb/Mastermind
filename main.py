bf69ed89fc23b31570d86f3ffc23bbbf9c659669
HugoErb <erb.hugo@yahoo.fr>
2023-09-02 22:06:21 +0200

    implementation de la meilleure combinaison à tester

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ def main():
                     user_choice = input()
                 try_res = check_colors(user_choice, nb_combinations, secret_colors)
         else:
-            all_combinations = remove_useless_combinations(all_combinations, user_choice, try_res[0], try_res[1], nb_combinations)
-            user_choice = random.choice(all_combinations)
+            all_combinations = remove_useless_combinations(all_combinations, try_res[0], try_res[1], nb_combinations, user_choice)
+            user_choice = define_next_combination_try(all_combinations)
             print(f'Le solveur à choisi {user_choice}')
             try_res = check_colors(user_choice, nb_combinations, secret_colors)
 
@@ -50,16 +50,17 @@ def main():
         print('\nPartie gagnée, bravo !')
         print(f'Nombre de tours : {nb_turns}')
 
-def remove_useless_combinations(all_combinations, nb_bien_places, nb_good_colors, nb_combinations, user_choice):
+def remove_useless_combinations(all_combinations: list, nb_bien_places: int, nb_good_colors: int, nb_combinations: int, user_choice: list) -> list:
     for combination in all_combinations:
         if check_colors(combination, nb_combinations, user_choice) != [nb_bien_places, nb_good_colors]:
             all_combinations.remove(combination)
+    print(f"Nombre de combinaisons restantes : {len(all_combinations)}")
     return all_combinations
     
 def check_colors(user_choice: list, nb_combinations: int, secret_colors: list) -> tuple:
     nb_bien_place = 0
     nb_good_color = 0
-    tmp_list = secret_colors.copy()
+    tmp_list = list(secret_colors).copy()
     for i in range(nb_combinations):
             # Check du nombre de couleurs bien placées
             if user_choice[i] == tmp_list[i]:
@@ -72,6 +73,30 @@ def check_colors(user_choice: list, nb_combinations: int, secret_colors: list) -
         
     return [nb_bien_place, nb_good_color]
 
+def define_next_combination_try(all_combinations):
+    # Initialise le score minimum à une valeur élevée
+    score_min = float('inf')
+    best_combination = None
+    
+    # Vérifie chaque combinaison possible
+    for combination in all_combinations:
+        # Pour chaque combinaison possible, calcule le nombre maximum de combinaisons restantes pour chaque réponse possible
+        max_restantes = 0
+        for reponse in itertools.product(range(5), repeat=2): # réponses de (0, 0) à (4, 4)
+            restantes = sum(1 for combo in all_combinations if reponse == obtenir_reponse(combination, combo))
+            max_restantes = max(max_restantes, restantes)
+        
+        # Si cette combination minimise le nombre maximum de combinaisons restantes, met à jour la meilleure combination et le score minimum
+        if max_restantes < score_min:
+            best_combination = combination
+            score_min = max_restantes
+            
+    return best_combination
+
+def obtenir_reponse(combination, combo):
+    rouges = sum(g == c for g, c in zip(combination, combo))
+    blancs = sum(g in combo for g in combination) - rouges
+    return rouges, blancs
 
 def check_string_composition(input_string, string_list, nb_combinations) -> bool:
     '''
